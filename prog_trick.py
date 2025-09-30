390c5c1db255623a90fe897c0ad1ca2bcc8ec3c5
Saeed Gholami <saeed.ghollami@gmail.com>
2025-10-01 03:14:07 +0330

    pacman packages order by install date.

---
+++ prog_trick.py
@@ -0,0 +1,19 @@
+
+# occipyed port
+port = None
+
+
+if port is None:
+    port = 8000
+
+print(port)
+
+
+
+port = None
+
+port = port or 8000
+
+print(port)
+
+
---
+++ sort_packages.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+
+
+packages_data = None
+with open("packages") as pkg_file:
+    packages_data = pkg_file.read()
+
+
+
+
+packages = packages_data.split("\n\n")
+
+packages_list = []
+
+for package in packages:
+    package_row = package.split("\n")
+    package_dict = {}
+    for row in package_row:
+        field, *values = row.split(" :")
+        field = field.strip()
+        if field in ["Name", "Install Date"]:
+            package_dict[field] = ''.join(values).strip()
+            packages_list.append(package_dict)
+
+        
+        
+packages = []
+
+date_format = '%a %d %b %Y %I:%M:%S %p %z'
+
+for pkg in packages_list:
+    name = pkg["Name"]
+    install_date = pkg["Install Date"] 
+    install_date = datetime.strptime(install_date, date_format)
+    packages.append({"Name": name, "Install Date": install_date})
+#
+
+sorted_pkgs = sorted(packages, key=lambda row: row["Install Date"])
+
+pkgs_name = [pkg["Name"] for pkg in sorted_pkgs]
+# pkgs = set(pkgs_name)
+
+# remove repreated packages
+pkgs = []
+for pkg in pkgs_name:
+    if pkg not in pkgs:
+        pkgs.append(pkg)
+
+for i, pkg in enumerate(pkgs):
+    print(i, pkg)
+
+
+
