c88b01cf4223e49f9242d7559bfddf19eceee38e
Saeed Gholami <saeed.ghollami@gmail.com>
2025-10-12 23:46:53 +0330

    update

---
+++ exer/format_num.py
@@ -0,0 +1,17 @@
+#
+# def create_phone_number(numbers):
+#     s = "".join(str(n) for n in numbers)
+#     part1 = "(" + s[0:3] + ")"
+#     part2 = " " + s[3:6] + "-"
+#     part3 = s[6:]
+#     result = part1 + part2 + part3
+#     return result
+
+
+def create_phone_number(numbers):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*numbers)
+
+
+# => returns "(123) 456-7890"
+x = create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) 
+print(x)
