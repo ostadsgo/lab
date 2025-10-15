a37a56a1d07eee00e93e2c38455b061c30229016
Saeed Gholami <saeed.ghollami@gmail.com>
2025-10-15 20:12:42 +0330

    video 03 - test code in python

--- exercisim/p3.py
+++
@@ -1,8 +0,0 @@
-
-# Create the get_change() function, taking 2 parameters:
-#
-#     budget : Amount of money before exchange.
-#     exchanging_value : Amount of money that is taken from the budget to be exchanged.
-
-def get_change(budget, exchanging_value):
-    return budget / exchanging_value
