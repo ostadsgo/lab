fe551b18976b33f4d7d3ff2ee383a6609afd1254
Saeed Gholami <saeed.ghollami@gmail.com>
2025-12-24 02:54:25 +0330

    Learn about c header file.

---
+++ c_header/calc.c
@@ -0,0 +1,8 @@
+#include "calc.h"
+
+
+
+int add(int x, int y)
+{
+    return x + y;
+}
---
+++ c_header/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "calc.h"
+
+
+
+int main()
+{
+    int result = add(2, 2);
+    printf("The rsult is %d\n", result);
+}
+
+
